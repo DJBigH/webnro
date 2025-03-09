04179afdd1f0f934e4e72707fc537110f184f453
bigh0405 <khanhbeotixiu8@gmail.com>
2025-03-09 20:16:20 +0700

    Done template clone nro

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $(".map").owlCarousel({
+        loop:true,
+        nav:false,
+        items: 1,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    });
+
+    $(".dulicate").owlCarousel({
+        loop:true,
+        nav:false,
+        items: 1,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    });
+
+    $(".boss").owlCarousel({
+        loop:true,
+        nav:false,
+        items: 1,
+        dots: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+    });
+  });
